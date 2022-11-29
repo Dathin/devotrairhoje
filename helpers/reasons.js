bf7329409df285ba337f580ed07f6ae898d6733e
Dathin <pedromcaires99@gmail.com>
2022-11-29 20:08:36 -0300

    melhorando as frases

--- helpers/reasons.js
+++ helpers/reasons.js
@@ -1,13 +1,14 @@
 export const REASONS_TO_DEPLOY = [
-  "SIM SIM SIM",
-  "Todo dia é dia",
-  'Melhor você do que ele(a)',
-  'Quem não faz toma',
-  'Especialistas recomendam pelo menos uma vez por dia',
-  'Quem não trai não ama',
-  'Só se o número do dia de hoje for divisível por 1',
-  'Só se vive uma vez, mais trair se trai todo dia',
-  'Um parceiro sem chifres é um parceiro indefeso',
-  'Por que não?',
-  'O que os olhos não veem...'  
+  "SIM SIM SIM!",
+  "Todo dia é dia!",
+  'Melhor você do que ele(a)!',
+  'Quem não faz toma!',
+  'Especialistas recomendam pelo menos uma vez por dia!',
+  'Quem não trai não ama!',
+  'Só se o número do dia de hoje for divisível por 1!',
+  'Só se vive uma vez, mais trair se trai todo dia!',
+  'Um parceiro sem chifres é um parceiro indefeso!',
+  'Por que não?!',
+  'O que os olhos não veem...!',
+  'Se você tem dúvidas é porque sim!'  
 ]
